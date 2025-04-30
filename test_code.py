11151292171095c2d381756c57fe5d2bb65c9819
gmahajan <gauravmahajan131@gmail.com>
2025-04-30 08:32:22 +0530

    Add all missing files

--- test_code.py
+++ test_code.py
@@ -15,6 +15,7 @@ def test_subtract_two_numbers():
     assert subtract_two_numbers(5, 3) == 2
     assert subtract_two_numbers(0, 0) == 0
     assert subtract_two_numbers(-1, -1) == 0
+    assert subtract_two_numbers(1, -1) == 2
 
 def test_multiply_two_numbers():
     assert multiply_two_numbers(2, 3) == 6
